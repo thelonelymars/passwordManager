114730fa81cdce47ab8380932b1705c30566611e
mshari <alshmmry923@gmail.com>
2024-06-16 09:22:04 +0300

    adding sign in page

--- passwordManagerFrontEnd/src/app/app.component.ts
+++ passwordManagerFrontEnd/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SignUpComponent,MainPageComponent],
+  imports: [RouterOutlet,SignUpComponent,MainPageComponent,SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
