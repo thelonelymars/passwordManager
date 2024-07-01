93dffccdc2a50388048a6fd48f4c63b804f60df3
mshari <alshmmry923@gmail.com>
2024-07-01 09:38:30 +0300

    modified:   passwordManagerFrontEnd/package-lock.json 	modified:   passwordManagerFrontEnd/package.json 	modified:   passwordManagerFrontEnd/src/app/app.config.ts

--- passwordManagerFrontEnd/src/app/app.config.ts
+++ passwordManagerFrontEnd/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
