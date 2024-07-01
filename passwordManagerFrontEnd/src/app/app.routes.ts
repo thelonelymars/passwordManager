348339a3673b196ea612d323cceeebf3e5524862
mshari <alshmmry923@gmail.com>
2024-07-01 09:35:53 +0300

    modified:   passwordManagerFrontEnd/src/app/app.component.html 	modified:   passwordManagerFrontEnd/src/app/app.component.ts 	modified:   passwordManagerFrontEnd/src/app/app.routes.ts 	new file:   passwordManagerFrontEnd/src/app/navigation.service.spec.ts 	new file:   passwordManagerFrontEnd/src/app/navigation.service.ts 	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.html 	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.ts 	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.html 	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.ts

--- passwordManagerFrontEnd/src/app/app.routes.ts
+++ passwordManagerFrontEnd/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'signup',component:SignUpComponent},{path:'signin', component:SignInComponent},{path:'mainpage', component:MainPageComponent}
+];
