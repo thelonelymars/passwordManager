e61428c463e5173302ba969caabd69fb4d5baf90
mshari <alshmmry923@gmail.com>
2024-07-02 08:18:00 +0300

    adding item cards components

--- passwordManagerFrontEnd/src/app/sign-up/sign-up.component.ts
+++ passwordManagerFrontEnd/src/app/sign-up/sign-up.component.ts
@@ -1,19 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  @Output() navigateRequest = new EventEmitter<string>();
-
+  @Input() isSignedIn: boolean | undefined;
+  @Output() isSignedInChange = new EventEmitter<boolean>()
   constructor(private router: Router) { }
 
   navigateToSignIn() {
+    this.isSignedIn = !this.isSignedIn;
+    this.isSignedInChange.emit(this.isSignedIn);
     this.router.navigateByUrl('/signin');
   }
+ 
+  
 }
