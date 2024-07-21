6bd5581ce0a49ad73fc64e91dfbb55887ef3e29a
mshari <alshmmry923@gmail.com>
2024-07-21 09:56:28 +0300

    adding drop down menu

--- passwordManagerFrontEnd/src/app/item-card/item-card.component.ts
+++ passwordManagerFrontEnd/src/app/item-card/item-card.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
+import { NgIf,NgFor } from '@angular/common';
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgFor, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './item-card.component.html',
   styleUrl: './item-card.component.css'
 })
 export class ItemCardComponent {
 
+  @Input() Title: string='';
+  @Input() Description: string='';
+
+   
 }
