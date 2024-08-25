a1a1d5d5f947bf8a194eb098a349649ab2b82222
thelonelymars <73229985+thelonelymars@users.noreply.github.com>
2024-08-25 10:24:06 +0300

    Test (#10)
    * adding sign in page
    
    * testing
    
    * testing
    
    * Your commit message
    
    * Integrated changes from orphan branch
    
    * modified:   passwordManagerFrontEnd/src/app/app.component.html
    	modified:   passwordManagerFrontEnd/src/app/app.component.ts
    	modified:   passwordManagerFrontEnd/src/app/app.routes.ts
    	new file:   passwordManagerFrontEnd/src/app/navigation.service.spec.ts
    	new file:   passwordManagerFrontEnd/src/app/navigation.service.ts
    	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.html
    	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.ts
    	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.html
    	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.ts
    
    * adding item cards components
    
    * testing sign commits
    
    * testing signed commits
    
    * modified:   passwordManagerFrontEnd/src/app/app.component.html
    	modified:   passwordManagerFrontEnd/src/app/app.component.ts
    	modified:   passwordManagerFrontEnd/src/app/app.routes.ts
    	new file:   passwordManagerFrontEnd/src/app/navigation.service.spec.ts
    	new file:   passwordManagerFrontEnd/src/app/navigation.service.ts
    	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.html
    	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.ts
    	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.html
    	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.ts
    
    * adding item cards components
    
    * testing sign commits
    
    * testing signed commits
    
    * modified:   passwordManagerFrontEnd/src/app/app.component.html
    	modified:   passwordManagerFrontEnd/src/app/app.component.ts
    	modified:   passwordManagerFrontEnd/src/app/app.routes.ts
    	new file:   passwordManagerFrontEnd/src/app/navigation.service.spec.ts
    	new file:   passwordManagerFrontEnd/src/app/navigation.service.ts
    	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.html
    	modified:   passwordManagerFrontEnd/src/app/sign-in/sign-in.component.ts
    	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.html
    	modified:   passwordManagerFrontEnd/src/app/sign-up/sign-up.component.ts
    
    * adding item cards components
    
    * adding drop down menu
    
    * testing
    
    * testing
    
    * Your commit message
    
    * Integrated changes from orphan branch
    
    * modified:   passwordManagerFrontEnd/src/app/item-card/item-card.component.html
    	modified:   passwordManagerFrontEnd/src/app/main-page/main-page.component.html
    
    * modified:   passwordManagerFrontEnd/src/app/main-page/main-page.component.html
    
    * adding drop down menu
    
    * testing
    
    * Your commit message
    
    * adding pop and add button
    
    * adding popup and add button
    
    * add logic to main page

--- passwordManagerFrontEnd/src/app/item-card/item-card.component.ts
+++ passwordManagerFrontEnd/src/app/item-card/item-card.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf, NgFor } from '@angular/common';
+
 
 import { NgIf,NgFor } from '@angular/common';
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [NgIf,NgFor, MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [NgIf, NgFor, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './item-card.component.html',
   styleUrl: './item-card.component.css'
 })
 export class ItemCardComponent {
 
-  @Input() Title: string='';
-  @Input() Description: string='';
 
-   
+  @Input() id: string = '';
+  @Input() Title: string = '';
+  @Input() Description: string = '';
+  @Input() CardInfo: string = '';
+  @Output() OutputCardInfo = new EventEmitter<string[]>();
+
+  SendCardInfo() {
+    let tempCardInfo = [];
+    
+    tempCardInfo.push(this.id);
+    tempCardInfo.push(this.Title);
+    tempCardInfo.push(this.Description);
+    this.OutputCardInfo.emit(tempCardInfo);
+    tempCardInfo = [];
+  }
 }
+
+
